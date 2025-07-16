932c42b6119ac4dfe560ca46d5ceb706b7cb660f
Julio De Aquino <deaquinojulio05@gmail.com>
2025-07-16 10:05:30 -0600

    dags(3): Arbeitnow Job Dag added

--- dags/utils/temp_request.py
+++ dags/utils/temp_request.py
@@ -1,10 +1,38 @@
 import requests
+import pandas as pd
 
-url = "https://www.arbeitnow.com/api/job-board-api"
+# API endpoint
+url = "https://www.arbeitnow.com/api/job-board-api?search=&tags=&sort_by=newest&page=2"
 
-payload={}
-headers = {}
+# Send GET request
+response = requests.get(url)
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# Confirm response type
+print(response.headers.get('Content-Type'))
 
-print(response.text)
+# Load JSON data
+data = response.json()
+
+# OPTIONAL: Pretty print to inspect structure
+from pprint import pprint
+pprint(data)
+
+# Convert to DataFrame (adjust path depending on JSON structure)
+df = pd.json_normalize(data['data'])  # assuming the jobs are under 'data'
+
+
+# Print DataFrame and columns
+print(df)
+print("\nColumns:")
+print(df.columns)
+
+print(df["slug"])
+print(df["company_name"])
+print(df["title"])
+print(df["description"])
+print(df["remote"])
+print(df["url"])
+print(df["tags"])
+print(df["job_types"])
+print(df["location"])
+print(df["created_at"])
